5e26ad0ab96a5e75164519136080d290d131005e
Arash Milani <me@arashmilani.com>
2012-04-30 20:42:42 +0430

    added some reset code in heading, paragraph and HtmlCode
    Signed-off-by: Arash Milani <me@arashmilani.com>

--- Demo.js
+++ Demo.js
@@ -1,5 +1,7 @@
 $(document).ready(function () {
-    var HtmlCode = "<p dir='ltr'>Representing a portion of an <strong>HTML</strong> " +
+    var HtmlCode = 
+        "<h2>Yet another editor?</h2>"+
+        "<p dir='ltr'>Representing a portion of an <strong>HTML</strong> " +
         "document and based on the <a href='#' style='color:red'>DOM Level 2</a> Range interface;</p>" +
         "<img src='Images/Paragraph.ClearFormatting.png' alt='Clear Formatting Icon'/>" +
         "<ul><li>My First Item</li><li>My First Item</li></ul>";
